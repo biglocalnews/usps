d5657b1edfd05482dfda3882c24ca6c842d9443e
Joe Nudell <jnu@stanford.edu>
2023-01-05 13:48:50 -0800

    simple search form

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
-  import {Heading} from 'flowbite-svelte';
+  import {Heading, Search, Button} from 'flowbite-svelte';
+
+  let query = '';
+
+  const search = (e) => {
+    e.preventDefault();
+    query = '';
+    // TKTK - run query
+    console.log(query);
+    return false;
+  };
 </script>
 
-<main>
-  <Heading>tktk</Heading>
+<main class="flex h-screen">
+  <div class="container m-auto p-16">
+    <header class="py-8">
+      <Heading>US Addresses Sampler</Heading>
+    </header>
+    <div>
+      <form id="search" on:submit={search}>
+        <Search bind:value={query}>
+          <Button type="submit">Sample</Button>
+        </Search>
+      </form>
+    </div>
+  </div>
 </main>
