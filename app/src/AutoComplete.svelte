4c1a24897e9d9bbce651f2314e1e9520809f405d
Joe Nudell <jnu@stanford.edu>
2023-01-12 09:51:26 -0800

    refactor components, improve ux of inputs

--- app/src/AutoComplete.svelte
+++ app/src/AutoComplete.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
-  export let options: Array<{name: string; value: any}> = [];
+  import type {ShapePointer} from './api.ts';
+  import {createEventDispatcher} from 'svelte';
+
+  /**
+   * Array of options to show.
+   */
+  export let options: ShapePointer[] = [];
+  /**
+   * Index of selected option.
+   */
   export let selected = 0;
+  /**
+   * Whether autocomplete component is visible.
+   */
+  export let visible = false;
+
+  const dispatch = createEventDispatcher();
+
+  // Handle selecting option with the mouse.
+  const handleClick = (e, i) => {
+    e.preventDefault();
+    dispatch('select', i);
+  };
 </script>
 
 <div>
   <slot />
-  <div class="absolute">
+  <div class="absolute" class:hidden={!visible}>
     <ul>
       {#each options as opt, i}
-        <li>
+        <li on:mousedown={(e) => handleClick(e, i)}>
           {opt.name}
           {#if i == selected}*{/if}
         </li>
