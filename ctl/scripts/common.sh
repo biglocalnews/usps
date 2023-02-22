4477e5ddc6cc54354f96fa91d80fedc9c3ed7df4
Joe Nudell <jnu@stanford.edu>
2023-02-21 21:42:31 -0800

    fix typo nh->nd

--- ctl/scripts/common.sh
+++ ctl/scripts/common.sh
@@ -9,7 +9,7 @@ fi
 # Expand 'all'
 if [ "$states" = "all" ]; then
     echo "Loading EVERY state!! This will take a very, very long time!"
-    export states="AL,AK,AZ,AR,CA,CO,CT,DE,DC,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,NH,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY"
+    export states="AL,AK,AZ,AR,CA,CO,CT,DE,DC,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY"
 fi
 
 # Make sure to use upper case. TIGER scripts don't work otherwise.
