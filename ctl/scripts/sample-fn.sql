693e4aa7016e4afc2a5dcb8f2e7a1e1c3fdd7365
Joe Nudell <jnu@stanford.edu>
2023-02-28 17:16:03 -0800

    improve query even more

--- ctl/scripts/sample-fn.sql
+++ ctl/scripts/sample-fn.sql
@@ -15,21 +15,18 @@ RETURNS TABLE (
     blkgrpce varchar(1)
 )
 LANGUAGE 'plpgsql'
+VOLATILE
+PARALLEL RESTRICTED
+LEAKPROOF
 AS
 $$
 DECLARE
-    fps record;
     local_addrs_sql text;
+    abbr text;
+    gpart record;
+    tcand record;
 BEGIN
-    DROP TABLE IF EXISTS local_tracts;
-
-    CREATE TEMP TABLE local_tracts AS
-    SELECT t.tract_id
-    FROM tract t
-    WHERE t.the_geom && g;
-
-    drop table if exists local_addrs;
-    create temp table local_addrs (
+    CREATE TEMP TABLE local_addrs (
         r float,
         hash varchar,
         point geometry,
@@ -42,24 +39,26 @@ BEGIN
         postcode varchar,
         tract_id varchar(11),
         blkgrpce varchar(1)
-    );
+    ) ON COMMIT DROP;
 
-    for fps in SELECT t.statefp, LOWER(s.stusps) as abbr
-                FROM (
-                    SELECT distinct substring(x.tract_id for 2) as statefp
-                    FROM local_tracts x
-                ) t LEFT JOIN state s
-                ON t.statefp = s.statefp
-    loop
-        local_addrs_sql = 'INSERT INTO local_addrs(r, hash, point, unit, number, street, city, district, region, postcode, tract_id, blkgrpce)'
-            ' SELECT random(), a.hash, a.point, a.unit, a.number, a.street, a.city, a.district, a.region, a.postcode, a.tract_id, a.blkgrpce'
-            ' FROM oa_%I a '
-            ' WHERE a.tract_id IN (SELECT t.tract_id FROM local_tracts t)';
-        EXECUTE format(local_addrs_sql, fps.abbr);
-        DELETE FROM local_addrs WHERE NOT ST_Contains(g, local_addrs.point);
-    end loop;
+    FOR gpart IN SELECT p FROM St_SubDivide(g) p
+    LOOP
+        FOR tcand IN SELECT t.tract_id FROM tract t WHERE t.the_geom && gpart.p
+        LOOP
+            SELECT LOWER(s.stusps) INTO abbr FROM state s WHERE s.statefp = substring(tcand.tract_id for 2);
+            local_addrs_sql = ''
+                'WITH sample AS ('
+                ' SELECT random(), a.hash, a.point, a.unit, a.number, a.street, a.city, a.district, a.region, a.postcode, a.tract_id, a.blkgrpce'
+                ' FROM oa_%I a '
+                ' WHERE a.tract_id = $1'
+                ')'
+                ' INSERT INTO local_addrs(r, hash, point, unit, number, street, city, district, region, postcode, tract_id, blkgrpce)'
+                ' SELECT s.* FROM sample s WHERE ST_Contains($2, s.point)';
+            EXECUTE format(local_addrs_sql, abbr) USING tcand.tract_id, gpart.p;
+        END LOOP;
+    END LOOP;
 
-    return query SELECT
+    RETURN QUERY SELECT
         a.hash,
         a.unit,
         a.number,
