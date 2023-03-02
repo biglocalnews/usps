a3eb7f9899c188a91b9b9f3d5c547193b58fd28f
Joe Nudell <jnu@stanford.edu>
2023-03-02 16:41:15 -0600

    support google analytics

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,29 @@
-<script>
+<script lang="ts">
+  import {browser} from '$app/environment';
   import '../app.postcss';
+  import {PUBLIC_GTAG} from '$env/static/public';
+  import {onMount} from 'svelte';
+
+  if (PUBLIC_GTAG) {
+    onMount(() => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+
+      gtag('config', PUBLIC_GTAG);
+    });
+  }
 </script>
+
+<svelte:head>
+  {#if PUBLIC_GTAG}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GTAG}"
+    ></script>
+  {/if}
+</svelte:head>
 
 <slot />
